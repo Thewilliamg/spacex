a45422fa4b050dd817a1c69acd9a1f9a3e725cfd
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-04-30 13:23:01 -0500

    feat: :construction: Se adelantan mas modulos en backend

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-import{ getAllMissions} from "./modules/missions.js";
+import { getAllRockets } from "./modules/rockets.js";
+import { 
+    progressRocketWeight, 
+    progressPayloadWeights, 
+    progressHeightRocket, 
+    progressDiameterRocket,
+    progressSecondStageDiameterRocket,
+    progressSecondStageHeightRocket
+} from "./modulesComponents/progressBar.js";
 
-let dataCrew
-let body = document.querySelector("body")
-dataCrew.forEach(val => {
-    plantilla+= `
-    <div>
-        <img src="${val.image}"referrerpolicy="no-referrer">
-        <p>${val.name}</p>
-    </div>` 
-})
+
+let information__2 = document.querySelector("#information__2");
+let dataRockets = await getAllRockets();
+let Totales = dataRockets.pop()
+
+let [Rockets1, Rockets2, Rockets3, Rockets4] = dataRockets; 
+
+console.log(dataRockets);
+console.log(Totales);
+
+information__2.append(...progressRocketWeight(Totales.kg_max, Rockets3))
+information__2.append(...progressPayloadWeights(Totales.payload_weights, Rockets3))
+information__2.append(...progressHeightRocket(Totales.height, Rockets3))
+information__2.append(...progressDiameterRocket(Totales.diameter, Rockets3))
+information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rockets3))
+information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rockets3))
